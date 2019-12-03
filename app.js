a4af76b4ef6aa7de959d71a51389899cfd072467
Michelle <sociocontacto.79@gmail.com>
2019-12-03 12:02:12 -0600

    segunda versión

--- app.js
+++ app.js
@@ -1,7 +1,34 @@
-window.onload()
+// Otra forma de ocultar con funciones aquí se ve
+const contenedor1 = document.getElementById('contenedor1');
+contenedor1.style.display = 'block';
 
-let name = document.getElementById('name-example');
+// Ahora no lo vemos
+// button.onclick = function saludo() {
+//     const name = document.getElementById('name').value;
+//     const parrafo = document.getElementById('saludo');
+//     parrafo.innerHTML = 'Hola ' + name;
+//     (`Hola, ${name}`);
+//     contenedor1.style.display = 'block';
+// }
 
-button.onclick = function saludo(name) {
-    alert(`Hola, ${name}`);
+function siguiente() {
+    const name = document.getElementById('name').value;
+    const saludo = document.getElementById('saludo');
+    saludo.innerHTML = '¡Hola, ' + name + '!';
+    contenedor1.style.display = 'none';
 }
+
+function escogerAmerica() {
+    preguntasAmerica.style.display = 'block'
+    preguntasEuropa.style.display = 'none'
+}
+
+function escogerEuropa() {
+    preguntasAmerica.style.display = 'none'
+    preguntasEuropa.style.display = 'block'
+}
+
+function resultadosFinales() {
+
+}
+// Ahora en vez de redirigir con cada página, mejor agrupar en divs y ocultar y mostrar
