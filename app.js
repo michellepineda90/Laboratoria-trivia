e94b2471185258e4d1bf34d781d32634e1491ec7
Michelle <sociocontacto.79@gmail.com>
2019-12-03 13:33:39 -0600

    tercera versión

--- app.js
+++ app.js
@@ -1,34 +1,36 @@
-// Otra forma de ocultar con funciones aquí se ve
-const contenedor1 = document.getElementById('contenedor1');
-contenedor1.style.display = 'block';
-
-// Ahora no lo vemos
-// button.onclick = function saludo() {
-//     const name = document.getElementById('name').value;
-//     const parrafo = document.getElementById('saludo');
-//     parrafo.innerHTML = 'Hola ' + name;
-//     (`Hola, ${name}`);
-//     contenedor1.style.display = 'block';
-// }
+const bienvenida = document.getElementById('bienvenida');
+bienvenida.style.display = 'block';
+const elige = document.getElementById('elige');
+elige.style.display = 'none';
+const preguntasAmerica = document.getElementById('preguntasAmerica');
+preguntasAmerica.style.display = 'none';
+const preguntasEuropa = document.getElementById('preguntasEuropa');
+preguntasEuropa.style.display = 'none';
+const enviarRespuestas = document.getElementById('enviarRespuestas');
+enviarRespuestas.style.display = 'none';
+const resultados = document.getElementById('resultados');
+resultados.style.display = 'none';
 
 function siguiente() {
     const name = document.getElementById('name').value;
     const saludo = document.getElementById('saludo');
     saludo.innerHTML = '¡Hola, ' + name + '!';
-    contenedor1.style.display = 'none';
+    bienvenida.style.display = 'none';
+    elige.style.display = 'block';
 }
 
 function escogerAmerica() {
-    preguntasAmerica.style.display = 'block'
-    preguntasEuropa.style.display = 'none'
+    elige.style.display = 'none';
+    preguntasAmerica.style.display = 'block';
+    preguntasEuropa.style.display = 'none';
 }
 
 function escogerEuropa() {
-    preguntasAmerica.style.display = 'none'
-    preguntasEuropa.style.display = 'block'
+    elige.style.display = 'none';
+    preguntasAmerica.style.display = 'none';
+    preguntasEuropa.style.display = 'block';
 }
 
 function resultadosFinales() {
 
 }
-// Ahora en vez de redirigir con cada página, mejor agrupar en divs y ocultar y mostrar
