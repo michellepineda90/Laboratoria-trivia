02b01d09a1710d0dac47e02e0d12096800a3a889
Michelle <sociocontacto.79@gmail.com>
2019-12-03 18:05:07 -0600

    Obtener valores del select

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const preguntasAmerica = document.getElementById('preguntasAmerica');
 preguntasAmerica.style.display = 'none';
 const preguntasEuropa = document.getElementById('preguntasEuropa');
 preguntasEuropa.style.display = 'none';
-const enviarRespuestas = document.getElementById('enviarRespuestas');
+const enviarRespuestas = document.getElementById('enviar');
 enviarRespuestas.style.display = 'none';
+const pregAmericaUno = document.getElementById('argentina');
+const pregAmericaDos = document.getElementById('elsalvador');
+const pregAmericaTres = document.getElementById('canada');
 const resultados = document.getElementById('resultados');
 resultados.style.display = 'none';
 
@@ -23,14 +26,20 @@ function escogerAmerica() {
     elige.style.display = 'none';
     preguntasAmerica.style.display = 'block';
     preguntasEuropa.style.display = 'none';
+    enviarRespuestas.style.display = 'block';
 }
 
 function escogerEuropa() {
     elige.style.display = 'none';
     preguntasAmerica.style.display = 'none';
     preguntasEuropa.style.display = 'block';
+    enviarRespuestas.style.display = 'block';
 }
 
-function resultadosFinales() {
-
+function resultado() {
+    if ((pregAmericaUno.value == "buenosaires") && (pregAmericaDos == "sansalvador") && (pregAmericaTres == "ottawa")) {
+        alert("Todas tus respuestas están correctas!")
+    } else {
+        alert("Oh no, inténtalo de nuevo")
+    }
 }
